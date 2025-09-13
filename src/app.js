e811b56b5d86a020b979567cafa2b7af3fc36c0c
hassan22m <hassan123maher22@gmail.com>
2025-09-13 13:16:54 +0300

    changed the app.js file

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ function greet(name) {
   return `Hello, ${name}!`;
 }
 
-module.exports = greet;
+module.exports = greet; // Export the function for use in other files
 
 if (require.main === module) {
   console.log(greet("World"));
